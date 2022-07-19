a766c641e3ca35c5c3dae7fdf40a5319bb73c70c
nattawat001 <108708210+nattawat001@users.noreply.github.com>
2022-07-19 11:31:50 +0700

    Update Zombie.cs

--- Assets/script/Zombie.cs
+++ Assets/script/Zombie.cs
@@ -3,22 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+
 public class Zombie : MonoBehaviour
 {
     NavMeshAgent agen;
     GameObject Player;
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         agen = GetComponent<NavMeshAgent>();
         Player = GameObject.Find("Player");
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   Float dist = Vector3.Distance(transform.position,Player.transform.position);
+        anim.SetFloat("distance",dist);
         if(Vector3.Distance(transform.position,Player.transform.position)<2.5f)
         {
+            
+            
             agen.speed = 0;
         }
         else agen.SetDestination(Player.transform.position);
